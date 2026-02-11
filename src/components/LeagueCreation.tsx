372bb5d68a3c01f0402340f239e63c9277583611
Amit Karwande <karwande@gmail.com>
2026-02-10 17:56:43 -0800

    event ended message

--- src/components/LeagueCreation.tsx
+++ src/components/LeagueCreation.tsx
@@ -1,3 +1,5 @@
+// LeagueCreation.tsx
+
 import { useState, type FormEvent, useEffect, useRef } from 'react';
 
 import { getGameConfig, isGameCompleted } from '../config/games';
@@ -118,9 +120,7 @@ export function LeagueCreation({ gameId }: LeagueCreationProps) {
                 d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
               />
             </svg>
-            <span>
-              This game has been completed. New leagues can no longer be created for this event.
-            </span>
+            <span>This event has ended. New leagues can no longer be created for this event.</span>
           </div>
           <p className="text-base-content/70 mt-4">
             You can still browse existing leagues if you have a league link. All data is available
